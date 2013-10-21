e5d5c69bcc0b2708d7e0dbaa1f3961fcc13176e3
Steve Potell <spotell@t-sciences.com>
2013-10-21 14:28:20 -0400

    added javadoc package info

---
+++ Evaluations/USAidEvaluations/src/com/tscience/usaid/evaluations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides the activity and fragment classes used by USAid Evaluations.
+ */
+package com.tscience.usaid.evaluations;
---
+++ Evaluations/USAidEvaluations/src/com/tscience/usaid/evaluations/utils/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides the utility classes used by USAid Evaluations.
+ */
+package com.tscience.usaid.evaluations.utils;
